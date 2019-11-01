aa80ab094cac89d19f0a0bb764396e1ad8d53740
burak-yildirim <yildirim.burak@protonmail.com>
2019-11-01 16:33:46 +0300

    SettingsScreen is not extending SimpleDialog anymore

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -2,79 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:my_xylophone/models/settings.dart';
 import 'package:provider/provider.dart';
 
-class SettingsScreen extends SimpleDialog {
-  SettingsScreen._init({@required context})
-      : super(
-          title: Text('Settings'),
-          children: <Widget>[
-            SimpleDialogOption(
-              onPressed: () {
-                Provider.of<Settings>(context).toggleNoteVisibility();
-              },
-              child: Consumer<Settings>(
-                builder: (ctx, settings, child) {
-                  return Text(
-                    settings.isNoteVisible ? 'Hide' : 'Show',
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        );
+class SettingsScreen {
+  static Widget _getDialog(BuildContext context) {
+    return SimpleDialog(
+      title: Text('Settings'),
+      children: <Widget>[
+        SimpleDialogOption(
+          onPressed: () {
+            Provider.of<Settings>(context).toggleNoteVisibility();
+          },
+          child: Consumer<Settings>(
+            builder: (ctx, settings, child) {
+              return Text(
+                settings.isNoteVisible ? 'Hide' : 'Show',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
   static void show(BuildContext buildContext) {
     showDialog(
       context: buildContext,
-      builder: (ctx) => SettingsScreen._init(context: ctx),
+      builder: (ctx) => _getDialog(ctx),
     );
   }
 }
-
-/*
-SimpleDialog(
-  title: Text('Settings'),
-  children: <Widget>[
-    SimpleDialogOption(
-      onPressed: () {
-        Provider.of<NoteVisibility>(context).toggleVisibility();
-      },
-      child: Consumer<NoteVisibility>(
-        builder: (ctx, noteVisibility, child) {
-          return Text(
-            noteVisibility.isVisible ? 'Hide' : 'Show',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          );
-        },
-      ),
-    ),
-  ],
-);
-*/
-
-/*
-Builder(
-  builder: (buildContext) {
-    return FlatButton(
-      color: Colors.green,
-      child: Consumer<NoteVisibility>(
-        builder: (ctx, noteVisibility, child) {
-          return Text(
-            noteVisibility.isVisible ? 'Hide' : 'Show',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          );
-        },
-      ),
-      onPressed: () {
-        Provider.of<NoteVisibility>(buildContext)
-            .toggleVisibility();
-      },
-    );
-  },
-)
-*/
